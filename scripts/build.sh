7f03b2c60651c7aacd1a3ee567018724c9d22701
Riccardo Pala <riccardo.pietro.pala@gmail.com>
2022-06-15 17:58:35 +0200

    Fixed build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,12 +5,12 @@ PROJECT_DIR=$(pwd)
 
 # echo $PROJECT_DIR
 
-for dir in */ ; do
-  [ -L "${d%/}" ] && continue
+for dir in `find . -type d -maxdepth 1`; do
   if [ -f $dir/package.json ]; then
     echo "# ################################################################# #"
     echo "  $dir contains a Javascript project"
     echo "# ################################################################# #"
     (cd $dir; npm install)
+    echo
   fi
 done
